c013396b041e385512eef3081fa4b7523fe35cef
USUARIO <USUARIO@USUARIO-PC>
2019-04-15 21:02:17 +0200

    1º migraciones BD

---
+++ database/migrations/2019_04_15_113722_create_clubs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClubsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clubs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',100);
+            $table->string('email',50)->unique();
+            $table->integer('phone_number');
+            $table->string('address');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clubs');
+    }
+}
